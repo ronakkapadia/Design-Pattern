5983f5b9617825a58b5abc068cd91c3e2fcdcecf
Ronak Kapadia <ronak_kapadia@jltasia.com>
2017-06-06 08:44:13 +0100

    Design Patterns

---
+++ AbstractFactoryPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using ComputerShop;
+using ComputerShop.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMachineFactory factory = new HighEndMachineFactory();// Or new LowEndMachineFactory();
+            
+            Shop shop = new Shop(factory);
+            shop.AssembleMachine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ComputerShop/Shop.cs
@@ -0,0 +1,33 @@
+﻿using ComputerShop.Devices;
+using ComputerShop.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerShop
+{
+    public class Shop
+    {
+        private IMachineFactory factory;
+
+        public Shop(IMachineFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        public void AssembleMachine()
+        {
+            IProcessor processor = factory.GetRam();
+            IHarddisk hdd = factory.GetHardDisk();
+            IMonitor monitor = factory.GetMonitor();
+            //use all three and create machine
+
+            processor.GetProcessor();
+            hdd.StoreData();
+            monitor.Display();
+
+        }
+    }
+}

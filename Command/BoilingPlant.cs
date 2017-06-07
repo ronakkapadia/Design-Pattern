1c99a11548c258f36c25242a2e8ed09e6484c08e
Ronak Kapadia <ronak_kapadia@jltasia.com>
2017-06-07 07:19:02 +0100

    Command Design pattern

---
+++ Command/BoilingPlant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Command
+{
+	public class BoilingPlant
+	{
+		public event EventHandler onBoiled; 
+		public void BoilWater()
+		{
+			for (int i = 0; i <= 150; i++)
+			{
+				Console.WriteLine(i);
+				Thread.Sleep(100);
+				if (i == 100)
+				{
+					onBoiled?.Invoke(null,null);
+				}
+			}
+		}
+	}
+}
---
+++ Command/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BoilingPlant bp = new BoilingPlant();
+			bp.onBoiled += Shutdown;
+			bp.onBoiled += Popup;
+			bp.onBoiled += CreateSound;
+
+			bp.BoilWater();
+			Console.ReadKey();
+
+		}
+
+		static void Shutdown(object sender, EventArgs e)
+		{
+			Console.WriteLine("Shutdown called");
+		}
+
+		static void Popup(object sender, EventArgs e)
+		{
+			Console.WriteLine("show pop up called");
+		}
+
+		static void CreateSound(object sender, EventArgs e)
+		{
+			Console.WriteLine("Create sound called");
+		}
+	}
+}
